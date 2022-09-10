5c7496f704a9e3e80f1aa2757eac99d95a843ec0
Svetozar Stojanovic <svetacar@gmail.com>
2022-09-10 18:14:23 +0200

    HTB-24 create logs directory if it does not exist

---
+++ src/definitions.py
@@ -0,0 +1,6 @@
+import os
+
+# absolute path to 'src' directory
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+# relative path to 'logs' directory from 'src' directory
+LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
--- src/util/logger.py
+++ src/util/logger.py
@@ -1,9 +1,15 @@
 # encoding: utf-8
+import os
 import logging
-
+from definitions import LOGS_DIR
 
 def init_logger(logfile: str):
-    """Initialize the root logger and standard log handlers."""
+    """! Initialize the root logger and standard log handlers."""
+    # check if logs directory exists, if not create it
+    
+    if not os.path.exists(LOGS_DIR):
+        os.mkdir(LOGS_DIR)
+
     log_formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
