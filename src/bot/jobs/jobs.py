9a4940a703e359e734a3e4150d8a465b9abd6a4a
Svetozar Stojanovic <svetacar@gmail.com>
2022-09-21 18:06:42 +0200

    HTB-14 add Telegram bot jobs module and jobs manager

---
+++ src/bot/jobs/jobs.py
@@ -0,0 +1,8 @@
+from telegram.ext import ContextTypes
+
+async def cleanup_inactive_user(context: ContextTypes.DEFAULT_TYPE):
+    pass
+async def update_hotspots(context: ContextTypes.DEFAULT_TYPE):
+    pass
+async def fetch_activities(context: ContextTypes.DEFAULT_TYPE):
+    pass
