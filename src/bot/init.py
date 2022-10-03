d0ec21661676f1de9c1fb03b64b3bb32d759c579
Svetozar Stojanovic <svetacar@gmail.com>
2022-10-03 19:29:27 +0200

    HTB-14 change init.py of bot to be concurrent and add additional handlers

--- src/bot/init.py
+++ src/bot/init.py
@@ -19,21 +19,24 @@
 #
 # Copyright (c) 2022 Svetozar Stojanovic.  All rights reserved.
 
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler
+from telegram.ext import ApplicationBuilder
 
+import zope.component
 from zope.generations.interfaces import ISchemaManager
 from zope.generations.generations import evolveMinimumSubscriber
 from zope.component import provideUtility
-from bot.db.DBManager import DBManager
+from telegram.ext._application import Application
+from telegram.ext._jobqueue import JobQueue
+
+from bot.db.DBUtil import DBUtil
 
 from bot.db.db_events import DatabaseOpenedEventStub
 from bot.handlers import *
+from bot.jobs.jobs import delete_stale_menu_nodes_for_user
 from util.read_secrets import read_secrets
 from bot.db.DBUpgradeSchemaManager import DBUpgradeSchemaManager
 
-from bot.ui.menu.MenuNode import MenuNode
-from bot.ui.items import *
-
+from bot.events.event_handlers import *
 from util.constants import DbConstants
 
 SECRETS = read_secrets()
@@ -44,7 +47,7 @@ def init_bot():
 
     @return An initialized Telegram Bot.
     """
-    application = ApplicationBuilder().token(SECRETS['BOT_TOKEN']).build()
+    application = ApplicationBuilder().token(SECRETS['BOT_TOKEN']).concurrent_updates(True).build()
     application.add_handlers(
         handlers=(
             # command handlers
@@ -55,7 +58,10 @@ def init_bot():
             hotspot_data_command_handler,
             hotspot_all_activity_command_handler,
             hotspot_recent_activity_command_handler,
-
+            roles_for_account_command_handler,
+            hotspot_activity_command_handler,
+            hotspot_rewards_command_handler,
+            
             # message handlers
             ui_message_handler,
         )
@@ -63,8 +69,15 @@ def init_bot():
 
     return application
 
+def init_handlers():
+    '''! Function for registering handlers for events. '''
+    zope.component.provideHandler(notification_event_handler)
+    zope.component.provideHandler(update_hotspot_event_handler)
+    zope.component.provideHandler(update_hotspots_globally_event_handler)
+
 def register_db_schema_manager():
     db_schema_manager = DBUpgradeSchemaManager()
     provideUtility(db_schema_manager, ISchemaManager, name=DbConstants.DB_APP_NAME)
-    evolveMinimumSubscriber(DatabaseOpenedEventStub(DBManager.get_db_ref()))
+    evolveMinimumSubscriber(DatabaseOpenedEventStub(DBUtil.get_db_ref()))
     return db_schema_manager
+
