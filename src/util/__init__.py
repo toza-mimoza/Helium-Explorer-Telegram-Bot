6a3596c47c1a6b15d1eb39ea118b3d01bfb171dc
Svetozar Stojanovic <svetacar@gmail.com>
2022-10-03 19:55:32 +0200

    HTB-14 add update helper util

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -3,3 +3,4 @@ from .formatter_helper import _bit, _it, _b, _ul, _st, _code_block, escape
 from .logger import init_logger
 from .read_secrets import read_secrets
 from .generate_hash import get_int64_hash
+from .update_helper import get_telegram_user_id
---
+++ src/util/update_helper.py
@@ -0,0 +1,2 @@
+def get_telegram_user_id(update):
+    return update.message.from_user.id
