7dd3bb92f9ab19f81d9ec56e761d96798c90594f
Svetozar Stojanovic <svetacar@gmail.com>
2022-10-03 19:27:22 +0200

    HTB-14 update main.py

--- src/main.py
+++ src/main.py
@@ -10,30 +10,38 @@
 # @section notes_init_bot Notes
 # - Bot is initialized using unique Telegram Bot ID.
 #
-# @section todo_init_bot TODO
-# - None.
-#
 # @section author_init_bot Author(s)
 # - Created by Svetozar Stojanovic on 08/23/2022.
-# - Modified by Svetozar Stojanovic on 08/23/2022.
+# - Modified by Svetozar Stojanovic on 09/16/2022.
 #
 # Copyright (c) 2022 Svetozar Stojanovic.  All rights reserved.
 
 import os
-from email.mime import application
+from bot.db.DBConnection import DBConnection
 from bot.db.DBManager import DBManager
+from bot.db.DBUtil import DBUtil
+from bot.jobs.jobs_manager import register_jobs
 import util.logger as logger
 from util.read_secrets import read_secrets
 from bot.init import init_bot
 from definitions import LOGS_DIR
-from bot.init import register_db_schema_manager
+from bot.init import register_db_schema_manager, init_handlers
+
 SECRETS = read_secrets()
 
 if __name__ == "__main__":
+    # initialize logger and set logs path
     filename = SECRETS['BOT_NAME'] + '.log'
     logger.init_logger(os.path.join(LOGS_DIR, filename))
     
+    # register schema manager and prepopulate DB
     db_schema_manager = register_db_schema_manager()
-    db_schema_manager.install(DBManager.get_db_ref().open())
+    db_schema_manager.install(DBConnection.connection)
+
+    # initialize event handlers
+    init_handlers()
+
+    # initialize bot
     application = init_bot()
+    register_jobs(application)
     application.run_polling()
