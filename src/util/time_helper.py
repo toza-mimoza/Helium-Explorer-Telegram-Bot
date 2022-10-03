b31085b8b04a12f23e1dc39d6e07f68bcde23db8
Svetozar Stojanovic <svetacar@gmail.com>
2022-10-03 19:34:56 +0200

    HTB-14 add functions for days ago time and getting time object from string

--- src/util/time_helper.py
+++ src/util/time_helper.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 """! @brief This function returns current UTC datetime in ISO format."""
 def get_iso_utc_time():
     return datetime.utcnow().isoformat()
@@ -6,4 +6,13 @@ def get_iso_utc_time():
 def get_time_diff_timedelta(t1, t2):
     time_pattern = '%Y-%m-%dT%H:%M:%S.%f'
     return datetime.strptime(t1, time_pattern) - datetime.strptime(t2, time_pattern)
-    
+
+def get_time_obj_from_str(time):
+    time_pattern = '%Y-%m-%dT%H:%M:%S.%f'
+    return datetime.strptime(time, time_pattern)
+
+def get_days_ago_time_str(days):
+    now = datetime.utcnow()
+    days_ago = now - timedelta(days=days)
+    return days_ago.isoformat()
+
