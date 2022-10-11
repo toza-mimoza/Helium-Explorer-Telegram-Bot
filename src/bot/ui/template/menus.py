98af7e9cb567ff0861b56af0d9a259538460ad16
Svetozar Stojanovic <svetacar@gmail.com>
2022-10-12 01:19:40 +0200

    HTB-8 add option for storing Menu Nodes in DB

--- src/bot/ui/template/menus.py
+++ src/bot/ui/template/menus.py
@@ -6,21 +6,31 @@ from .keyboards import *
 def build_main_menu(telegram_user_id):
     if not DBUtil.is_user_registered(telegram_user_id):
         keyboard = main_menu_keyboard_var_setup
-        return MenuNode(label='', telegram_user_id=telegram_user_id, ui_object=keyboard, previous=None)
-
+        menu = MenuNode(label=UiLabels.UI_LABEL_MAIN_MENU, telegram_user_id=telegram_user_id, ui_object=keyboard, previous=None)
+        return menu
+    
     if DBUtil.is_bot_active(telegram_user_id):
         keyboard = main_menu_keyboard_var_stop
     else:
         keyboard = main_menu_keyboard_var_start
-    return MenuNode(label=UiLabels.UI_LABEL_MAIN_MENU, telegram_user_id=telegram_user_id, ui_object=keyboard, previous=None)
+
+    menu = MenuNode(label=UiLabels.UI_LABEL_MAIN_MENU, telegram_user_id=telegram_user_id, ui_object=keyboard, previous=None)
+    #menu.update()
+    return menu
 
 def build_sub_menu_settings(telegram_user_id):
-    return MenuNode(label=UiLabels.UI_LABEL_MENU_SETTINGS, telegram_user_id=telegram_user_id, ui_object=settings_sub_menu_keyboard)
+    menu = MenuNode(label=UiLabels.UI_LABEL_MENU_SETTINGS, telegram_user_id=telegram_user_id, ui_object=settings_sub_menu_keyboard)
+    # menu.update()
+    return menu
 
 def build_sub_menu_snooze(telegram_user_id):
-    return MenuNode(label=UiLabels.UI_LABEL_MENU_SNOOZE, telegram_user_id=telegram_user_id, ui_object=snooze_sub_menu_keyboard)
+    menu = MenuNode(label=UiLabels.UI_LABEL_MENU_SNOOZE, telegram_user_id=telegram_user_id, ui_object=snooze_sub_menu_keyboard)
+    # menu.update()
+    return menu
 
 def build_sub_menu_overview(telegram_user_id):
-    return MenuNode(label=UiLabels.UI_LABEL_MENU_OVERVIEW, telegram_user_id=telegram_user_id, ui_object=overview_sub_menu_keyboard)
+    menu = MenuNode(label=UiLabels.UI_LABEL_MENU_OVERVIEW, telegram_user_id=telegram_user_id, ui_object=overview_sub_menu_keyboard)
+    # menu.update()
+    return menu
 
 
