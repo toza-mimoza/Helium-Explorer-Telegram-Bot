e9464364760eca47cc6e328df642c9ae9b01dff1
Svetozar Stojanovic <svetacar@gmail.com>
2022-10-12 01:17:39 +0200

    HTB-14 separate default sending message actions from others

---
+++ src/bot/message_actions.py
@@ -0,0 +1,20 @@
+
+from telegram import Update
+from telegram.ext import ContextTypes
+from telegram.constants import ParseMode
+
+
+async def send_message(text: str, update: Update, context: ContextTypes):
+    '''! Text that is not a command will be echoed back to the user.
+    '''
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.MARKDOWN)
+
+async def send_mdv2_message(text: str, update: Update, context: ContextTypes):
+    '''! Text that is not a command will be echoed back to the user.
+    '''
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.MARKDOWN_V2)
+
+async def send_html_message(text: str, update: Update, context: ContextTypes):
+    '''! Text that is not a command will be echoed back to the user.
+    '''
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.HTML)
